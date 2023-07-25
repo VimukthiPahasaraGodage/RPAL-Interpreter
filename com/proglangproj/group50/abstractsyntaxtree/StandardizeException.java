c8adc0af548fb25c8a24d4009a230fd62459381d
ASUS-USER <dinindu.20@cse.mrt.ac.lk>
2023-07-26 01:35:46 +0530

    ASTNode,ASTNodeType and StandardizeException_1

--- com/proglangproj/group50/abstractsyntaxtree/StandardizeException.java
+++ com/proglangproj/group50/abstractsyntaxtree/StandardizeException.java
@@ -1,9 +1,7 @@
 package com.proglangproj.group50.abstractsyntaxtree;
 
-import java.io.Serial;
 
 public class StandardizeException extends RuntimeException{
-  @Serial
   private static final long serialVersionUID = 1L;
   
   public StandardizeException(String message){
